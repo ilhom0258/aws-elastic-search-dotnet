d17df3e5b4cc06797208c476f645a40b98e50ad6
Buriev Ilhomjon <ilhom0258@gmail.com>
2021-06-04 23:04:09 +0500

    Created models for properties and management

---
+++ AWSElasticSearchIntegration/Core/Models/Management.cs
@@ -0,0 +1,10 @@
+namespace AWSElasticSearchIntegration.Core.Models
+{
+    public class Mgmt
+    {
+        public int MgmtID { get; set;}
+        public string Name { get; set; }
+        public string Market { get; set; }
+        public string State { get; set; }     
+    }
+}
---
+++ AWSElasticSearchIntegration/Core/Property.cs
@@ -0,0 +1,15 @@
+namespace AWSElasticSearchIntegration.Core
+{
+    public class Property
+    {
+        public int PropertyID { get; set; }
+        public string Name { get; set; }
+        public string FormerName { get; set;}
+        public string StreetAddress { get; set; }
+        public string City { get; set; }
+        public string Market { get; set; }
+        public string State { get; set; }
+        public string Lat { get; set;}
+        public string Lng { get; set; }
+    }
+}
